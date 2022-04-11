1a3a7ef761190fc5f6dc83248d23649fe578f9c4
young <youngsun0401@naver.com>
2022-04-11 18:23:42 +0900

    calc 만듦

---
+++ src/calc/Calc.java
@@ -0,0 +1,7 @@
+package calc;
+
+public class Calc {
+	public int add( int a, int b ) {
+		return a+b;
+	}
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,15 @@
 package main;
 
+import calc.Calc;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("안녕하세요");
-
+		Calc c = new Calc();
+		System.out.println( c.add(1, 3));
+		
 	}
 
 }
